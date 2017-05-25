7ca217e63be5ec658fa04b7b1153b810a5111545
djili <sndevnb1@gmail.com>
2017-05-25 14:59:26 +0000

    adding pymongo support

--- service.py
+++ service.py
@@ -1,10 +1,29 @@
 import json
+import pymongo
 from bottle import request, route, run
+
 
 @route('/',method='POST')
 def index()
+
+     # getting data
      data=request.params.get('data')
      print "your data = {data}".format(data=data)
+
+     # establish a connection to the database
+     connection = pymongo.MongoClient("mongodb://localhost")
+
+     # get a handle to the bigdata
+     db = connection.bigdata
+     sensor = db.sensor
+
+
+     # insert data
+     try:
+         sensor.insert_one(data)
+     except Exception as e:
+         print "Unexpected error:", type(e), e
+
      return "sucess"
 
 run(host='0.0.0.0', port=8080, debg=True)
